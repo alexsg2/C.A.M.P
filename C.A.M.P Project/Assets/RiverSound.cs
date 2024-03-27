3cb32d74568ba6aac8b5dfb643fcfde95e5f65ea
Felix Lopez <143753704+fe1ixlopez@users.noreply.github.com>
2024-03-27 13:00:36 -0400

    Added River, Background, & Campfire Audio

---
+++ C.A.M.P Project/Assets/RiverSound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Cinemachine
+{
+    public class RiverSound : MonoBehaviour
+    {
+        public CinemachinePathBase m_Path; // path/track (river) to follow
+        public GameObject Player; // player to follow
+
+        float m_Position;       // The position along the path to set the cart to in path units
+        private CinemachinePathBase.PositionUnits m_PositionUnits = CinemachinePathBase.PositionUnits.PathUnits;
+
+        void Update()
+        {
+            // Find closest point to the player along the path
+            SetCartPosition(m_Path.FindClosestPoint(Player.transform.position, 0, -1, 10));
+        }
+
+        // Set cart's position to closest point
+        void SetCartPosition(float distanceAlongPath)
+        {
+            m_Position = m_Path.StandardizeUnit(distanceAlongPath, m_PositionUnits);
+            transform.position = m_Path.EvaluatePositionAtUnit(m_Position, m_PositionUnits);
+            transform.rotation = m_Path.EvaluateOrientationAtUnit(m_Position, m_PositionUnits);
+        }
+    }
+}

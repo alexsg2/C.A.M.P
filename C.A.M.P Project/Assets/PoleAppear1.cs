41760c6705cf55147fdb60404884ff1702e56ef6
Alexander Georgiev <alex.s.georgiev@gmail.com>
2024-04-15 19:10:02 -0400

    Final Tent Changes for the Day

--- C.A.M.P Project/Assets/PoleAppear1.cs
+++ C.A.M.P Project/Assets/PoleAppear1.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PoleAppear1 : MonoBehaviour
 {
     public GameObject pole1; // reference to standing pole1
+    private bool triggered = false; // flag to track if trigger has been activated
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Pole"))
+        if (other.CompareTag("Pole") && !triggered)
         {
             Destroy(other.gameObject);
             pole1.SetActive(true);
+            triggered = true; // set the flag to true
         }
     }
 }
--- C.A.M.P Project/Assets/PoleAppear2.cs
+++ C.A.M.P Project/Assets/PoleAppear2.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PoleAppear2 : MonoBehaviour
 {
     public GameObject pole2; // reference to standing pole1
+    private bool triggered = false; // flag to track if trigger has been activated
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Pole"))
+        if (other.CompareTag("Pole") && !triggered)
         {
             Destroy(other.gameObject);
             pole2.SetActive(true);
+            triggered = true; // set the flag to true
         }
     }
 }

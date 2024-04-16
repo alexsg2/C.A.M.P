a8d173dd3fd2e4c6c412d067e63cd7bcb32ac1f4
Evan Lee <evanlee010110@gmail.com>
2024-04-16 16:00:32 -0400

    Text
    speech to text

--- C.A.M.P Project/Assets/Scripts/NPC Control.cs
+++ C.A.M.P Project/Assets/Scripts/NPC Control.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.InputSystem;
 
 public class NPCControl : MonoBehaviour
 {
     [SerializeField] private GameObject Camera;
     [SerializeField] private GameObject toActivate;
 
-    [SerializeField] private GameObject Testing;
+    private Transform avatar;
+
 
     private async void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            await Task.Delay(50);
+
             // enable the diolog
             toActivate.SetActive(true);
 
@@ -23,6 +28,7 @@ public class NPCControl : MonoBehaviour
 
     public void Recover()
     {
+        // avatar.GetComponent<PlayerInput>().enabled = true;
         toActivate.SetActive(false);
     }
 }

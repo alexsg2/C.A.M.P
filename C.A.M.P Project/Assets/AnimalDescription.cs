08cc153d87bf54e770ac2853efa0302c657e5017
sophyaharg <94576382+sophyaharg@users.noreply.github.com>
2024-04-24 13:56:22 -0400

    Fixed Issues Alex made

--- C.A.M.P Project/Assets/AnimalDescription.cs
+++ C.A.M.P Project/Assets/AnimalDescription.cs
@@ -1,25 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
-using System.Collections;
-using System.Collections.Generic;
 
 public class AnimalDescription : MonoBehaviour
 {
     public Text canvasText;
-    public TaskList taskList;
-    public GameObject AnimalText;
-    private bool selectedLandAnimal = false;
-    private bool selectedBird = false;
 
+    public TaskList taskList;
+
+    public GameObject AnimalText;
+
+    private bool selectedLandAnimal = false;
+
+    private bool selectedBird = false;
 
     private void Start()
     {
         var interactable = GetComponent<XRBaseInteractable>();
-        interactable.hoverEntered.AddListener(OnHoverEnter);
-        interactable.hoverExited.AddListener(OnHoverExit);
-        interactable.selectEntered.AddListener(OnSelectEnter);
-        interactable.selectExited.AddListener(OnSelectExit);
+        interactable.hoverEntered.AddListener (OnHoverEnter);
+        interactable.hoverExited.AddListener (OnHoverExit);
+        interactable.selectEntered.AddListener (OnSelectEnter);
+        interactable.selectExited.AddListener (OnSelectExit);
     }
 
     private void OnHoverEnter(HoverEnterEventArgs args)
@@ -50,6 +53,7 @@ public class AnimalDescription : MonoBehaviour
         canvasText.text = "";
         AnimalText.SetActive(false);
     }
+
     private string GetAnimalName()
     {
         string animalName = gameObject.name;
@@ -85,13 +89,13 @@ public class AnimalDescription : MonoBehaviour
             }
             else
             {
-                return "The California Red-legged Frog:\n\nA striking amphibian native to California's coastal regions, known for its distinct red coloring on its legs. Found in various aquatic habitats like ponds and streams, they feed on insects and small vertebrates. Breeding occurs in spring and summer, with females laying eggs in shallow water. Once widespread, habitat loss, pollution, and introduced predators have led to their decline. They're now a threatened species, vital for ecosystem health, requiring conservation efforts for their survival.";
+                return "The California Red-legged Frog:\n\nA striking amphibian native to California's coastal regions, known for its distinct red coloring on its legs. Found in various aquatic habitats like ponds and streams, they feed on insects and small vertebrates. Breeding occurs in spring and summer, with females laying eggs in shallow water. They're now a threatened species, vital for ecosystem health, requiring conservation efforts for their survival.";
             }
         }
         else if (animalName.Contains("Deer"))
         {
             taskList.updateT3Sub1();
-            return "White-Tailed Deer:\n\nGraceful and elegant mammals. They are found inhabiting various environments such as forests, grasslands, and even suburban areas. Deer are known for their slender bodies, long legs, and distinct antlers (in most species, but not all) found on males. Their fur can vary in color depending on the species and the season, ranging from reddish-brown to grayish-brown.";
+            return "White-Tailed Deer:\n\nGraceful and elegant mammals. They are found inhabiting various environments such as forests, grasslands, and even suburban areas. Deer are known for their slender bodies, long legs, and distinct antlers found on males. Their fur can vary in color depending on the species and the season.";
         }
         else if (animalName.Contains("Turtle"))
         {
@@ -101,37 +105,37 @@ public class AnimalDescription : MonoBehaviour
         else if (animalName.Contains("blueJay"))
         {
             taskList.updateT3Sub2();
-            return "Blue Jay\nBlue jays are primarily blue and relatively small birds, with white undersides and chests. They have a black U-shaped collar around the neck.";
+            return "Blue Jay\nBlue jays are primarily blue and relatively small birds, with white undersides and chests. They have a black U-shaped collar around the neck. They are large, intelligent, and colorful songbirds. They can also live in various habitats and have complex social systems that involve tight family bonds.";
         }
         else if (animalName.Contains("cardinal"))
         {
             taskList.updateT3Sub2();
-            return "Cardinal\nCardinals are medium-sized thick-billed songbirds, many with crested heads and some bright red coloring.";
+            return "Cardinal\nCardinals are medium-sized thick-billed songbirds, many with crested heads and some bright red coloring. They have strong breaks for crushing seeds, long tails, and prominent crests. Cardinals tend to sit in a hunched over position with their tails pointing towards the ground.";
         }
         else if (animalName.Contains("chickadee"))
         {
             taskList.updateT3Sub2();
-            return "Chickadee\nChickadees are small, plump birds with round bodies and short tails. They have black heads and backs, with white cheeks and undersides.";
+            return "Chickadee\nChickadees are small, plump birds with round bodies and short tails. They have black heads and backs, with white cheeks and undersides. Chickadees are very agile and they are often seen hopping around on branches in search of food. They eat a variety of insects and seeds.";
         }
         else if (animalName.Contains("crow"))
         {
             taskList.updateT3Sub2();
-            return "Crow\nThere are a wide variety of crow species. Most crows have black-colored feathers with an iridescent shine to them.";
+            return "Crow\nThere are a wide variety of crow species. Most crows have black-colored feathers with an iridescent shine to them. They often live together in large families and are known for their loud voices and intelligence. Crows are curious birds.";
         }
         else if (animalName.Contains("goldFinch"))
         {
             taskList.updateT3Sub2();
-            return "Gold Finch\nGoldfinches are songbirds with short, notched tails and lots of yellow coloring. They are migratory birds.";
+            return "Gold Finch\nGoldfinches are songbirds with short, notched tails and lots of yellow coloring. They are migratory birds. All have rather delicate sharp-pointed bills for finches. Flocks of goldfinches feed in fields and gardens.";
         }
         else if (animalName.Contains("robin"))
         {
             taskList.updateT3Sub2();
-            return "Robin\nRobins are small to medium-sized birds belonging to the thrush family, Turdidae. They are known for their distinctive orange-red breast, which contrasts with their grayish-brown upperparts. Robins have a relatively large head with a prominent black eye and a slender bill. They are widespread across North America and Europe, with some species found in other parts of the world as well. Robins are highly adaptable birds, inhabiting various environments such as woodlands, gardens, parks, and even urban areas. They feed primarily on insects, earthworms, fruits, and berries. Robins are known for their cheerful songs, which they use to establish territories and attract mates, especially during the breeding season.During nesting season, which typically begins in spring, female robins construct cup-shaped nests using grass, twigs, and mud, often located in trees, shrubs, or even on ledges. They lay a clutch of eggs, usually blue or greenish-blue in color, and both parents take turns incubating the eggs and feeding the young.";
+            return "Robin\nRobins are small to medium-sized birds with an unmistakable orange breast and face. They have gray plumage on either side of the orange breast. Robins have sturdy legs with muscles designed for running or hopping.";
         }
         else if (animalName.Contains("sparrow"))
         {
             taskList.updateT3Sub2();
-            return "Sparrow\nSparrows are small birds, usually brown and gray. They have short tails and small, strong beaks. ";
+            return "Sparrow\nSparrows are small birds, usually brown and gray. They have short tails and small, strong beaks. Sparrows are social birds and they live in flocks. It's beak is short and thick to better crack open seeds.";
         }
 
         return animalName;
